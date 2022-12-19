7db127bb3a3f2abe0528660a77dd27740252a836
kastourik12 <kastouriw@yahoo.fr>
2022-12-19 12:34:48 +0100

    add redirect api call
    	- add redirect api call
    	- displaying statistics before redirecting

--- src/services/short-url.service.js
+++ src/services/short-url.service.js
@@ -3,5 +3,8 @@ import axios from "axios";
 export default {
     async createShortUrl(url){
         return await axios.post("/re/create",url)
+    },
+     getRedirectionUrl(path){
+      return axios.get(path)
     }
 }
